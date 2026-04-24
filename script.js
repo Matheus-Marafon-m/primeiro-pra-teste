a9ae92b5fd031928146215e42ef0758e650576ee
Matheus Marafon <mtamborini@minsait.com>
2026-04-24 19:56:19 -0300

    Fix - Remove script in HTML / fixed arquivo.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const startBtn = document.getElementById("start-btn");
+const btn = document.getElementById("start-btn");
+const screen = document.getElementById("start-screen");
+const app = document.getElementById("app");
+
+app.style.display = "none";
+
+btn.addEventListener("click", () => {
+    screen.style.display = "none";
+    app.style.display = "block";
+});
+
+
+
+/*const startBtn = document.getElementById("start-btn");
 const startScreen = document.getElementById("start-screen");
 const app = document.getElementById("app");
 
 startBtn.addEventListener("click", () => {
 startScreen.style.display = "none";
 app.style.display = "block";
-});
+});*/
